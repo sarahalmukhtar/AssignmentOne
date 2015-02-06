74c4d9e2d201e9ab44e416d8721738491053a72b
sarahalmukhtar <sarah.almukhtar@gmail.com>
2015-02-06 14:44:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+//Things I like to do in my free time
+var freetime = ["Radiolab", "Sacred Economics", "Only Lovers Left Alive"];
+
+freetime.forEach(
+	function printValue(value) {
+		console.log("I like", value);
+	}
+);
